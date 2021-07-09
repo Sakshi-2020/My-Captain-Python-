b90797e5d7456e13e2a172222a49ef0f24c32350
Sakshi Singh <68339450+Sakshi-2020@users.noreply.github.com>
2021-07-09 22:40:07 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 21:46:05 2021
+
+@author: ssing
+"""
+
+from math import pi
+r= float(input("Input the radius of the circle : "))
+area= pi*r*r
+print('The area of the circle with radius ', r, ' is: ', area)
