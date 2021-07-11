6f8833b7cd326edb646957f33dae947d6bff1cca
Sakshi Singh <68339450+Sakshi-2020@users.noreply.github.com>
2021-07-11 22:16:44 +0530

    Add files via upload

---
+++ file_extension.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 11 22:13:41 2021
+
+@author: ssing
+"""
+
+
+fn= input("Input the Filename: ")
+f = fn.split('.')
+print ("The extension of the file is : " + f[-1])
